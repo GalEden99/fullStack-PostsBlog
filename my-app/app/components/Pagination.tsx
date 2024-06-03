56b9ad2cfc34a668e27a286e71f4320aec5f7f1e
omer2080 <omer208010@gmail.com>
2024-06-03 14:57:16 +0300

    implemented fetching 10 posts per page

--- my-app/app/components/Pagination.tsx
+++ my-app/app/components/Pagination.tsx
@@ -1,18 +1,35 @@
 //pagination component
 import React from "react";
 
-export default function Pagination() {
+export default function Pagination(props: {
+  page: number;
+  onClick: (id: number) => void;
+}) {
+  // console.log(props);
+  // console.log(props.page);
+  // console.log(props.onChange);
+
   return (
     <div className="pagination">
       <a href="#">&laquo;</a>
-      <a href="#">1</a>
-      <a className="active" href="#">
+      <a id="1" href="#" onClick={() => props.onClick(1)}>
+        1
+      </a>
+      <a id="2" href="#" className="active" onClick={() => props.onClick(2)}>
         2
       </a>
-      <a href="#">3</a>
-      <a href="#">4</a>
-      <a href="#">5</a>
-      <a href="#">6</a>
+      <a id="3" href="#" onClick={() => props.onClick(3)}>
+        3
+      </a>
+      <a id="4" href="#" onClick={() => props.onClick(4)}>
+        4
+      </a>
+      <a id="5" href="#" onClick={() => props.onClick(5)}>
+        5
+      </a>
+      <a id="6" href="#" onClick={() => props.onClick(6)}>
+        6
+      </a>
       <a href="#">&raquo;</a>
     </div>
   );
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,31 +2,40 @@
 
 import React, { useEffect, useState } from "react";
 import Pagination from "./components/Pagination";
+import { pages } from "next/dist/build/templates/app-page";
 
 interface Post {
   id: number;
   title: string;
   author: string;
 }
-
-async function getPosts() {
-  const response = await fetch("http://localhost:3001/posts");
-  if (!response.ok) {
-    throw new Error(`Error! status: ${response.status}`);
-  }
-  const data = await response.json();
-  return data;
-}
+const URL = "http://localhost:3001/posts";
 
 export default function Home() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
+
+  function handleChangedPage(id: number) {
+    setPage(id);
+  }
+
+  async function getPosts() {
+    const response = await fetch(
+      URL + "?id_gte=" + (10 * page - 9) + "&id_lte=" + 10 * page
+    );
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  }
 
   useEffect(() => {
     getPosts()
       .then((data) => setPosts(data))
       .catch((err) => setError(err.message));
-  }, []);
+  });
 
   return (
     <div>
@@ -39,11 +48,12 @@ export default function Home() {
             <li key={post.id}>
               <h2>{post.title}</h2>
               <p>{post.author}</p>
+              <p>{post.id}</p>
             </li>
           ))}
         </ul>
       )}
-      <Pagination />
+      <Pagination page={page} onClick={handleChangedPage} />
     </div>
   );
 }
